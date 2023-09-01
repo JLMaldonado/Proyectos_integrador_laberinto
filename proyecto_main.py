5ef93b714a0ff515fcd79431fe36be18ef24ac87
Jose luis Maldonado Padilla <joseluismaldonado2020@gmail.com>
2023-08-31 20:12:45 -0600

    actualizacion del proyecto

--- proyecto_main.py
+++ proyecto_main.py
@@ -1,2 +1,7 @@
+print("==========================================")
+print("Bienvenido al Juego de laberinto")
+print("============================================")
+
 nombre=input("Ingresa tu nombre de usuario por favor: ")
+
 print(f"Bienvenido al Juego {nombre} Que disfrutes")
--- readmi.md
+++ readmi.md
@@ -1,1 +1,3 @@
-esta es la primer parte del proyecto laberinto
+Bienvinidos a mi proyecto de laberinto 
+aqui estare trabajando parte por parte hasta finalizar el proyecto del 
+juego laberinto 
