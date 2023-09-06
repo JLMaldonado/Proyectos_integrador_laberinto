ce8ba080ce5c031a117b312749d7d9b19f0f87de
Jose luis Maldonado Padilla <joseluismaldonado2020@gmail.com>
2023-09-05 18:31:26 -0600

    actualizacion parte 2

--- proyecto_main.py
+++ proyecto_main.py
@@ -5,3 +5,20 @@ print("============================================")
 nombre=input("Ingresa tu nombre de usuario por favor: ")
 
 print(f"Bienvenido al Juego {nombre} Que disfrutes")
+
+from readchar import readkey, key
+
+import keyboard
+print("======================================")
+print("Lector de teclas por teclado")
+print("======================================")
+print("Presiona la tecla (↑ para salir):")
+
+while True:
+    k=readkey()
+
+    if k==key.UP :
+        break
+    print(f"Tecla presionada: {k}")
+
+print("fin")
